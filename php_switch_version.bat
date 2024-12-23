562f088b34d2488cd23842f6af0a3d1db8ad1dab
Dawid 'DeyV' Polak <dawid.polak@mentax.pl>
2024-12-23 13:00:13 +0100

    php 8.4

--- php_switch_version.bat
+++ php_switch_version.bat
@@ -1,7 +1,47 @@
 @ECHO off
-ECHO zmiana wersji php 8.1 - 8.2
+ECHO zmiana wersji php 8.1 - 8.2 - 8.3
 
-IF exist php82 ( ren php php81 && ren php82 php ) ELSE ( ren php php82 && ren php81 php )
+set /p Ver=<version.txt
+
+ECHO Poprzednia wersja PHP bylo %Ver%
+ECHO:
+
+IF %Ver%==84 goto :PHP84
+IF %Ver%==83 goto :PHP83
+IF %Ver%==82 goto :PHP82
+IF %Ver%==81 goto :PHP81
+
+
+:PHP81
+    REN php php81
+    REN php82 php
+
+    echo 82 > version.txt
+    goto :FINISH
+
+:PHP82
+    REN php php82
+    REN php83 php
+
+    echo 83 > version.txt
+    goto :FINISH
+
+:PHP83
+    REN php php83
+    REN php84 php
+
+    echo 84 > version.txt
+    goto :FINISH
+
+:PHP84
+    REN php php84
+    REN php81 php
+
+    echo 81 > version.txt
+    goto :FINISH
+
+
+:FINISH
 
 ECHO Aktualna wersja PHP TO:
 
